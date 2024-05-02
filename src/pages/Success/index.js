45e5dab6af67ce05c54d09a1f82c6d720dfc2c8f
ELMANNY4000 <ulodoe@gmail.com>
2024-05-02 02:50:52 +0100

    add

---
+++ src/pages/Success/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import "./success.css"
+
+export const index = () => {
+  return (
+    <>
+        <div class="done">
+      <main class="done-inner">
+        <div class="frame-parent">
+          <div class="frame-group">
+            <div class="frame-wrapper">
+              <div class="frame-container">
+                <div class="frame-child"></div>
+                <div class="frame-item"></div>
+              </div>
+            </div>
+            <div class="industrial-designer-working-3d-parent">
+              <img
+                class="industrial-designer-working-3d-icon"
+                loading="lazy"
+                alt=""
+                src="./public/industrialdesignerworking3dmodel-2-1@2x.png"
+              />
+
+              <div class="current-level-inform-container">
+                <ul class="report-fraud-cases-search-and">
+                  <li class="report-fraud-cases">Report fraud cases</li>
+                  <li class="search-and-identify">
+                    Search and identify fake accounts
+                  </li>
+                  <li>Real-time actionable and informed decision</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="metric-item-wrapper">
+            <div class="metric-item">
+              <img
+                class="efs-1-icon"
+                loading="lazy"
+                alt=""
+                src="./public/7efs-1@2x.png"
+              />
+
+              <div class="frame-div">
+                <div class="success-parent">
+                  <h1 class="success">Success!</h1>
+                  <div class="your-account-has">
+                    Your account has been successfully created
+                  </div>
+                </div>
+                <button class="mds-tw-button1">
+                  <div class="button-text1">Continue to dashboard</div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    </>
+  )
+}
