206c5be8ff5b211ccf7407768fa704af159c4e36
ELMANNY4000 <ulodoe@gmail.com>
2024-05-03 05:18:41 +0100

    add

--- component/popup/Popup.js
+++ component/popup/Popup.js
@@ -1,12 +1,84 @@
-import React from 'react'
-import "./popup.css"
+import React from "react";
+import "./popup.css";
 
 export const Popup = () => {
   return (
     <>
-     <div className='popup'>
+      <div className="popup">
+        <section class="status-bar-parent">
+          <div class="frame-wrapper5">
+            <div class="frame-parent13">
+              <div class="frame-wrapper6">
+                <div class="metric-item3">
+                  <div class="heading8">
+                    <div class="vector-container">
+                      <img
+                        class="vector-icon2"
+                        loading="lazy"
+                        alt=""
+                        src="./public/vector@2x.png"
+                      />
 
-     </div>
+                      <div class="welcome-seun-container">
+                        <h3 class="welcome-seun2">Welcome Seun,</h3>
+                        <div class="your-account-is2">
+                          Your account is all set!
+                        </div>
+                      </div>
+                    </div>
+                    <div class="button2">
+                      <img
+                        class="secondaryoutlinebx-x-icon2"
+                        loading="lazy"
+                        alt=""
+                        src="./public/secondaryoutlinebxx.svg"
+                      />
+                    </div>
+                  </div>
+                  <div class="value-frame">
+                    <b class="value2">Getting started on Rensecure</b>
+                  </div>
+                  <div class="accessing-your-account-group">
+                    <b class="accessing-your-account2">
+                      Accessing your account
+                    </b>
+                    <div class="from-now-on2">
+                      From now on , you will use Johndoe@gmail,com to access
+                      your account
+                    </div>
+                  </div>
+                  <div class="frame-parent14">
+                    <div class="report-a-fraud-container">
+                      <b class="report-a-fraud2">Report a fraud</b>
+                      <div class="you-can-easily2">
+                        You can easily report a fraud case, and help other
+                        people avoid scam and fraudsters out there
+                      </div>
+                    </div>
+                    <button class="mds-tw-button12">
+                      <div class="button-text12">Report a fraud</div>
+                    </button>
+                  </div>
+                  <div class="stay-up-to-date-on-latest-risk-group">
+                    <b class="stay-up-to2">
+                      Stay up to date on latest risk news
+                    </b>
+                    <div class="we-have-curated2">
+                      We have curated a list of contents and tools to help you
+                      make the right decision
+                    </div>
+                  </div>
+                  <div class="mds-tw-button-frame">
+                    <button class="mds-tw-button13">
+                      <div class="button-text13">Got it!</div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
