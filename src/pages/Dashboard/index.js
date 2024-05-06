34740ec00127e12b6873583b72a66adf6e793667
ELMANNY4000 <ulodoe@gmail.com>
2024-05-06 14:50:35 +0100

    add

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,72 +3,72 @@ import React from "react";
 const index = () => {
   return (
     <>
-      <div class="dashboard">
-        <main class="navigation-parent">
-          <section class="frame-wrapper2">
-            <div class="frame-parent4">
-              <div class="frame-parent5">
-                <div class="current-level-inform-parent">
-                  <div class="current-level-inform">Welcome Seun,</div>
-                  <div class="heading-parent">
-                    <h2 class="heading3">
+      <div className="dashboard">
+        <main className="navigation-parent">
+          <section className="frame-wrapper2">
+            <div className="frame-parent4">
+              <div className="frame-parent5">
+                <div className="current-level-inform-parent">
+                  <div className="current-level-inform">Welcome Seun,</div>
+                  <div className="heading-parent">
+                    <h2 className="heading3">
                       Intelligent database to protect you from fraud!
                     </h2>
-                    <div class="current-level-inform1">
+                    <div className="current-level-inform1">
                       What would you like to do today?
                     </div>
                   </div>
-                  <div class="heading-group">
-                    <h2 class="heading4">Report a fraud case</h2>
-                    <div class="we-fetch-all">
+                  <div className="heading-group">
+                    <h2 className="heading4">Report a fraud case</h2>
+                    <div className="we-fetch-all">
                       We fetch all the possible matches and their individual
                       details for your search results
                     </div>
-                    <button class="mds-tw-button9">
-                      <div class="button-text9">Report now</div>
+                    <button className="mds-tw-button9">
+                      <div className="button-text9">Report now</div>
                     </button>
-                    <div class="layers-minimalistic-svgrepo-co-parent">
+                    <div className="layers-minimalistic-svgrepo-co-parent">
                       <img
-                        class="layers-minimalistic-svgrepo-co-icon"
+                        className="layers-minimalistic-svgrepo-co-icon"
                         loading="lazy"
                         alt=""
                         src="./public/layersminimalisticsvgrepocom.svg"
                       />
 
                       <img
-                        class="frame-child2"
+                        className="frame-child2"
                         alt=""
                         src="./public/group-55323.svg"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="frame-parent6">
-                  <div class="frame-wrapper3">
-                    <div class="frame-parent7">
-                      <div class="frame-child3"></div>
-                      <div class="frame-child4"></div>
+                <div className="frame-parent6">
+                  <div className="frame-wrapper3">
+                    <div className="frame-parent7">
+                      <div className="frame-child3"></div>
+                      <div className="frame-child4"></div>
                     </div>
                   </div>
-                  <div class="mds-tw-button-parent">
-                    <button class="mds-tw-button10">
-                      <div class="button-text10">Search our database</div>
+                  <div className="mds-tw-button-parent">
+                    <button className="mds-tw-button10">
+                      <div className="button-text10">Search our database</div>
                     </button>
-                    <div class="card-search-svgrepo-com-1-parent">
+                    <div className="card-search-svgrepo-com-1-parent">
                       <img
-                        class="card-search-svgrepo-com-1-icon"
+                        className="card-search-svgrepo-com-1-icon"
                         loading="lazy"
                         alt=""
                         src="./public/cardsearchsvgrepocom-1.svg"
                       />
 
-                      <h2 class="heading5">Run a search</h2>
-                      <div class="we-fetch-all1">
+                      <h2 className="heading5">Run a search</h2>
+                      <div className="we-fetch-all1">
                         We fetch all the possible matches and their individual
                         details for your search results
                       </div>
                       <img
-                        class="frame-child5"
+                        className="frame-child5"
                         alt=""
                         src="./public/group-55323-1.svg"
                       />
@@ -76,104 +76,104 @@ const index = () => {
                   </div>
                 </div>
               </div>
-              <div class="frame-parent8">
-                <div class="heading-container">
-                  <h1 class="heading6">Fraud and risk focus blog</h1>
-                  <div class="input-title-component">
-                    <div class="appointment-reminder">
-                      <div class="data-for-good-logo">
+              <div className="frame-parent8">
+                <div className="heading-container">
+                  <h1 className="heading6">Fraud and risk focus blog</h1>
+                  <div className="input-title-component">
+                    <div className="appointment-reminder">
+                      <div className="data-for-good-logo">
                         <img
-                          class="data-for-good-logo-child"
+                          className="data-for-good-logo-child"
                           loading="lazy"
                           alt=""
                           src="./public/frame-282658@2x.png"
                         />
 
-                        <b class="why-financial-wellbeing">
+                        <b className="why-financial-wellbeing">
                           Why financial wellbeing can be the key to combatting
                           the real cost of Christmas
                         </b>
                       </div>
-                      <div class="december-2023">18 December 2023</div>
+                      <div className="december-2023">18 December 2023</div>
                     </div>
-                    <div class="appointment-reminder1">
-                      <div class="frame-parent9">
+                    <div className="appointment-reminder1">
+                      <div className="frame-parent9">
                         <img
-                          class="frame-icon"
+                          className="frame-icon"
                           alt=""
                           src="./public/frame-282658-1@2x.png"
                         />
 
-                        <b class="data-for-good">
+                        <b className="data-for-good">
                           Data for Good: Resecure’s Role in the Future Data
                           Landscape
                         </b>
                       </div>
-                      <div class="december-20231">18 December 2023</div>
+                      <div className="december-20231">18 December 2023</div>
                     </div>
-                    <div class="appointment-reminder2">
-                      <div class="frame-parent10">
+                    <div className="appointment-reminder2">
+                      <div className="frame-parent10">
                         <img
-                          class="frame-child6"
+                          className="frame-child6"
                           alt=""
                           src="./public/frame-282658-2@2x.png"
                         />
 
-                        <b class="the-online-safety">
+                        <b className="the-online-safety">
                           The Online Safety Act: Landmark legislation on online
                           fraud
                         </b>
                       </div>
-                      <div class="december-20232">18 December 2023</div>
+                      <div className="december-20232">18 December 2023</div>
                     </div>
                   </div>
                 </div>
-                <div class="social-connect">
-                  <div class="company-about-parent">
-                    <div class="company-about">
-                      <h1 class="frequently-asked-questions-container">
-                        <p class="frequently">Frequently</p>
-                        <p class="asked-questions">asked questions</p>
+                <div className="social-connect">
+                  <div className="company-about-parent">
+                    <div className="company-about">
+                      <h1 className="frequently-asked-questions-container">
+                        <p className="frequently">Frequently</p>
+                        <p className="asked-questions">asked questions</p>
                       </h1>
-                      <div class="everything-you-need-container">
-                        <p class="everything-you-need">
+                      <div className="everything-you-need-container">
+                        <p className="everything-you-need">
                           Everything you need to know about relocating
                         </p>
-                        <p class="to-germany">to germany</p>
+                        <p className="to-germany">to germany</p>
                       </div>
                     </div>
-                    <button class="mds-tw-button11">
-                      <div class="button-text11">Read more FAQ</div>
+                    <button className="mds-tw-button11">
+                      <div className="button-text11">Read more FAQ</div>
                     </button>
                   </div>
-                  <div class="content-area">
-                    <div class="component-5">
-                      <div class="why-financial-wellbeing-sub-he-wrapper">
-                        <div class="why-financial-wellbeing-sub-he">
-                          <b class="input-title">How does it work?</b>
+                  <div className="content-area">
+                    <div className="component-5">
+                      <div className="why-financial-wellbeing-sub-he-wrapper">
+                        <div className="why-financial-wellbeing-sub-he">
+                          <b className="input-title">How does it work?</b>
                           <img
-                            class="why-financial-wellbeing-sub-he-child"
+                            className="why-financial-wellbeing-sub-he-child"
                             alt=""
                             src="./public/frame-7386@2x.png"
                           />
                         </div>
                       </div>
                     </div>
-                    <div class="component-6">
-                      <div class="button-instance">
-                        <div class="input-title-parent">
-                          <b class="input-title1">
+                    <div className="component-6">
+                      <div className="button-instance">
+                        <div className="input-title-parent">
+                          <b className="input-title1">
                             How can you open a blocked account
                           </b>
                           <img
-                            class="frame-child7"
+                            className="frame-child7"
                             alt=""
                             src="./public/frame-7386-1@2x.png"
                           />
                         </div>
                       </div>
-                      <div class="when-your-appointment-container">
-                        <p class="when-your-appointment">
+                      <div className="when-your-appointment-container">
+                        <p className="when-your-appointment">
                           When your appointment date starts drawing nigh, you
                           need to start considering opening a blocked bank
                           account. This is essentially your proof of funds
@@ -184,8 +184,8 @@ const index = () => {
                           and lets you withdraw only €861 monthly when you
                           arrive in Germany.
                         </p>
-                        <p class="blank-line">&nbsp;</p>
-                        <p class="once-you-get">
+                        <p className="blank-line">&nbsp;</p>
+                        <p className="once-you-get">
                           Once you get admitted to a German school, it becomes
                           easier from there. However, remember that €10,322 is
                           no small amount. So, you want to be careful and
@@ -194,14 +194,14 @@ const index = () => {
                           documents you typically need to open a German
                           blocked account:
                         </p>
-                        <ul class="admission-letter-from-a-univer">
-                          <li class="admission-letter-from-a-univer1">
+                        <ul className="admission-letter-from-a-univer">
+                          <li className="admission-letter-from-a-univer1">
                             <b>Admission letter from a university</b>
                           </li>
-                          <li class="pre-paid-fee-valid-internatio">
+                          <li className="pre-paid-fee-valid-internatio">
                             <b>Pre-paid fee- Valid international passport- </b>
                           </li>
-                          <li class="a-filled-out-application-form">
+                          <li className="a-filled-out-application-form">
                             <b>
                               A filled out application form (find details in the
                               next step)-
@@ -216,36 +216,36 @@ const index = () => {
                         </ul>
                       </div>
                     </div>
-                    <div class="component-7">
-                      <div class="component-7-inner">
-                        <div class="input-title-group">
-                          <b class="input-title2">How to apply for admission</b>
+                    <div className="component-7">
+                      <div className="component-7-inner">
+                        <div className="input-title-group">
+                          <b className="input-title2">How to apply for admission</b>
                           <img
-                            class="frame-child8"
+                            className="frame-child8"
                             alt=""
                             src="./public/frame-7386@2x.png"
                           />
                         </div>
                       </div>
                     </div>
-                    <div class="component-8">
-                      <div class="component-8-inner">
-                        <div class="input-title-container">
-                          <b class="input-title3">How to apply for admission</b>
+                    <div className="component-8">
+                      <div className="component-8-inner">
+                        <div className="input-title-container">
+                          <b className="input-title3">How to apply for admission</b>
                           <img
-                            class="frame-child9"
+                            className="frame-child9"
                             alt=""
                             src="./public/frame-7386@2x.png"
                           />
                         </div>
                       </div>
                     </div>
-                    <div class="component-9">
-                      <div class="component-9-inner">
-                        <div class="input-title-parent1">
-                          <b class="input-title4">How to apply for admission</b>
+                    <div className="component-9">
+                      <div className="component-9-inner">
+                        <div className="input-title-parent1">
+                          <b className="input-title4">How to apply for admission</b>
                           <img
-                            class="frame-child10"
+                            className="frame-child10"
                             alt=""
                             src="./public/frame-7386@2x.png"
                           />
